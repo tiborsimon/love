29238348f1424ab21922989b76a5d34d6317ae4a
Tibor Simon <tibor@tiborsimon.io>
2016-09-16 00:07:39 +0200

    Working web app.

--- src/js/client.js
+++ src/js/client.js
@@ -1,1 +1,26 @@
-console.log("since is working..")
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Since extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      title: "1000",
+    };
+  }
+
+  changeTitle(title) {
+    this.setState({title});
+  }
+
+  render() {
+    return (
+      <div>
+        <p>We are {this.state.title} days old.</p>
+      </div>
+    );
+  }
+}
+
+const app = document.getElementById('app');
+ReactDOM.render(<Since/>, app);
